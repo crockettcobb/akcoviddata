6aec4a8d6bc3efe73c3b8d1327f961b94bfd8460
Crockett Cobb <crockett.cobb@gmail.com>
2021-02-27 09:23:59 -0900

    read cases update

--- scripts/read.py
+++ scripts/read.py
@@ -35,6 +35,7 @@ def parse_cases():
 
     cases_map ={
         'OnsetDate' : 'date',
+        # 'ReportDate' : 'date',
         'Econ_Name' : 'region',
         'Resident' : 'resident',
     }
@@ -70,50 +71,51 @@ def parse_cases():
 
     return
 
-def read_and_parse():
+
+def read_reportdate():
+    ''' Read and parse the cases counts for resident and nonresidents
     '''
-    This will load data from the Alaska State Department of Health and 
-    Human Services  and parse it into a standard format CSV 
-    '''
 
-    cases_url = 'https://opendata.arcgis.com/datasets/f2b5073959c247368e4cd28e54cd0cff_0.csv'
-
-    nonres_cases_url = 'https://opendata.arcgis.com/datasets/f34c6b1e58f34939bb6d2d721eb7a1e7_0.csv'
-    report_date_url = 'https://opendata.arcgis.com/datasets/6ed020fa516948b2983e36bba817b1b7_0.csv'
-
-    df_cases= pd.read_csv(cases_url, parse_dates=['Date_'])
-    df_nonres_cases = pd.read_csv(nonres_cases_url, parse_dates=['ReportDate'])
-    df_report_date = pd.read_csv(report_date_url, parse_dates=['ReportDate'])
-
+    case_url = 'https://opendata.arcgis.com/datasets/44002fca49af4d5e918174b6acdd92ea_0.csv'
+    df_cases = pd.read_csv(case_url, parse_dates=['ReportDate'])
     df_cases.to_csv('../data/raw/cases.csv', index=False)
-    df_nonres_cases.to_csv('../data/raw/nonres_cases.csv', index=False)
-    df_report_date.to_csv('../data/raw/reportdate_cases.csv')
 
-    nonres_cases_map = {
+    cases_map ={
         'ReportDate' : 'date',
-        'FID' : 'nonresident_cases',
-        'Temporary_Region' : 'region',
+        'Econ_Name' : 'region',
+        'Resident' : 'resident',
     }
 
-    report_date_cases_map = {
-    'ReportDate' : 'date',
-    'FID' : 'cases',
-    'Region' : 'region',
-    }
+    df_cases = df_cases[list(cases_map.keys())].copy()
+    df_cases = df_cases.rename(columns=cases_map)
+    df_cases['date'] = pd.to_datetime(df_cases['date']).dt.date
 
-    df_report_date = df_report_date.groupby(['ReportDate', 'Region']).count().copy().reset_index()
-    df_report_date = df_report_date.rename(columns=report_date_cases_map)
-    df_report_date['region'] = df_report_date['region'].str.replace('Matanuska-Susitna', 'MatSu')
-    df_report_date['date'] = pd.to_datetime(df_report_date['date']).dt.date
-    df_report_date[['date', 'region', 'cases']].to_csv('../data/parsed/report_date_cases.csv', index=False)
+    df_res_cases = df_cases[df_cases['resident']=='Y'].copy()
+    df_res_cases = df_res_cases.groupby(['date', 'region']).count().copy().reset_index()
+    df_res_cases = pd.pivot_table(df_res_cases, columns=['region'], values='resident', index='date').fillna(0)
 
-    df = df_cases.merge(df_nonres, left_on=['date', 'region'], right_on=['date', 'region'])
-    df['total_cases'] = df[['cases', 'nonresident_cases']].sum(axis=1)
-    df.to_csv('../data/parsed/total_cases.csv', index=False)
+    regions = df_res_cases.columns.tolist()
+    df_res_cases['Statewide'] = df_res_cases[regions].sum(axis=1)
+    df_res_cases = pd.melt(df_res_cases.reset_index(), id_vars='date')
+    df_res_cases.columns = ['date', 'region', 'resident']
 
-    return
+    df_nonres_cases = df_cases[df_cases['resident']=='N'].copy()
+    df_nonres_cases = df_nonres_cases.groupby(['date', 'region']).count().copy().reset_index()
+    df_nonres_cases = pd.pivot_table(df_nonres_cases, columns=['region'], values='resident', index='date').fillna(0)
 
+    regions = df_nonres_cases.columns.tolist()
+    df_nonres_cases['Statewide'] = df_nonres_cases[regions].sum(axis=1)
+    df_nonres_cases = pd.melt(df_nonres_cases.reset_index(), id_vars='date')
+    df_nonres_cases.columns = ['date', 'region', 'nonresident']
+    
+    df_cases = df_res_cases.merge(df_nonres_cases, left_on=['date','region'], right_on=['date', 'region'], how='outer').fillna(0)
+    df_cases['total'] = df_cases[['resident', 'nonresident']].sum(axis=1)
+    df_cases[['total', 'resident', 'nonresident']] = df_cases[['total', 'resident', 'nonresident']] .astype(int)
+    df_cases['region'] = df_cases['region'].str.replace('Matanuska-Susitna', 'MatSu')
+
+    df_cases.to_csv('../data/parsed/report_date.csv', index=False)
 
 if __name__ == '__main__':
     # parse_tests()
     parse_cases()
+    read_reportdate()
